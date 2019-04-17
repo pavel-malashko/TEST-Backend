8533d858ca08a021368feafbb3780c663db93962
Pavel <pavelmalashko57@gmail.com>
2019-04-17 13:32:25 +0300

    remove node_modules & dist

--- dist/router.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var _cosntants = require('./cosntants');
-
-_cosntants.app.get('/v1/articles', article.index);
-_cosntants.app.post('/v1/articles', article.create);
-_cosntants.app.get('/v1/articles/:id', article.read);
-_cosntants.app.put('/v1/articles/:id', article.update);
